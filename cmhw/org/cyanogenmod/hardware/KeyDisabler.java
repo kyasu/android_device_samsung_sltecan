f16baa7057d998e3022f163ee4cf536718cda648
kyasu <kyasu.dev@gmail.com>
2016-08-18 09:02:46 +0900

    cmhw: Use cmsdk FileUtils

--- cmhw/org/cyanogenmod/hardware/KeyDisabler.java
+++ cmhw/org/cyanogenmod/hardware/KeyDisabler.java
@@ -16,9 +16,7 @@
 
 package org.cyanogenmod.hardware;
 
-import java.io.File;
-
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 /*
  * Disable capacitive keys
@@ -31,30 +29,33 @@ import org.cyanogenmod.hardware.util.FileUtils;
 
 public class KeyDisabler {
 
-    private static String CONTROL_PATH = "/sys/class/sec/sec_touchkey/keypad_enable";
-
+    private static String KEYDISABLER_PATH = "/sys/class/sec/sec_touchkey/keypad_enable";
     /*
      * All HAF classes should export this boolean.
      * Real implementations must, of course, return true
      */
+
     public static boolean isSupported() {
-        File f = new File(CONTROL_PATH);
-        return f.exists();
+        return FileUtils.isFileWritable(KEYDISABLER_PATH);
     }
 
     /*
      * Are the keys currently blocked?
      */
+
     public static boolean isActive() {
         int i;
-        i = Integer.parseInt(FileUtils.readOneLine(CONTROL_PATH));
+        i = Integer.parseInt(FileUtils.readOneLine(KEYDISABLER_PATH));
+
         return i > 0 ? false : true;
     }
 
     /*
      * Disable capacitive keys
      */
+
     public static boolean setActive(boolean state) {
-        return FileUtils.writeLine(CONTROL_PATH, String.valueOf(state ? 0 : 1));
+        return FileUtils.writeLine(KEYDISABLER_PATH, String.valueOf(state ? 0 : 1));
     }
+
 }
