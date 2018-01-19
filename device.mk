40f3738ecbde1166da862bf5d3ff0648de6ee009
kyasu <kyasu.dev@gmail.com>
2018-01-19 14:55:12 +0900

    sltecan: Move nfcee_access.xml back to etc

--- device.mk
+++ device.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfcee_access.xml:system/vendor/etc/nfcee_access.xml \
+    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml \
     $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/vendor/etc/libnfc-nxp.conf
 
